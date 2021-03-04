356cf7f8073fea728ecbc85b50a84750d16d1290
Marta <marta.gilabert.guzman@gmail.com>
2021-03-04 17:23:43 +0100

    merge

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -3,8 +3,30 @@ import {Redirect} from 'react-router-dom'
 import axios from 'axios'
 import config from '../config'
 import { LinearProgress } from "@material-ui/core";
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles({
+    root: {
+      maxWidth: 500,
+      margin: 6,
+      marginBottom: 10,
+    },
+    title: {
+      fontSize: 14,
+    },
+    pos: {
+      marginBottom: 12,
+    }
+  });
+
 
 function Profile(props) {
+    const classes = useStyles();
 
     const [error, setError] = useState(null)
     const [userPost, setUserPost] = useState([])
@@ -39,7 +61,44 @@ function Profile(props) {
 
              {
                  userPost.map((singlePost)=>{
-                     return <p> THIS IS AN POST :{singlePost.description}</p>
+                     if(singlePost.postType == 'post'){
+                         return (
+                            <Card className={classes.root}>
+                            <CardContent>
+                            <Typography variant="body2" component="p">
+                            {singlePost.description}
+                            <br />
+                            </Typography>
+                        </CardContent>
+                        <CardActions>
+                        <Button size="small">Delete Post</Button>
+                        </CardActions>
+                        </Card>
+
+                         )
+
+                     } 
+                     
+                     else {
+                         return (
+                        <Card className={classes.root}>
+                            <CardContent>
+                            <Typography className={classes.pos} color="textSecondary">
+                            {singlePost.title}
+                            {singlePost.postType}
+                            </Typography>
+                            <Typography variant="body2" component="p">
+                            {singlePost.description}
+                            <br />
+                            </Typography>
+                        </CardContent>
+                        <CardActions>
+                        <Button size="small">Delete Post</Button>
+                        </CardActions>
+                        </Card>
+                        )
+                     }
+
                  })
              }
 
